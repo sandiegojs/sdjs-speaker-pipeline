0348935bb1709b241abb0103cd5c49eed5def651
Austin Gray <austin.gray4292@gmail.com>
2018-11-06 16:24:10 -0800

    fixes date format on meetup axios call (#37)

--- server/utils/getMeetups.js
+++ server/utils/getMeetups.js
@@ -5,7 +5,7 @@ function getMeetups() {
 	return new Promise((resolve, reject) => {
 		let currentDate = new Date();
 		currentDate.setMonth(currentDate.getMonth() + 3);
-		axios.get(`https://api.meetup.com/sandiegojs/events?no_later_than=${currentDate.toLocaleDateString()}`)
+		axios.get(`https://api.meetup.com/sandiegojs/events?no_later_than=${currentDate.getFullYear()}-${currentDate.getMonth()}-${currentDate.getDay()}`)
 			.then(response => {
 				resolve(response.data.map(event => ({
 					meetupId: event.id,
