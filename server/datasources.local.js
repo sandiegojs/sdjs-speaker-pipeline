0f7c0a4d6f98011c351e3f1fd322505bbd998920
Anthony Valera <anthony@anthonyvalera.com>
2019-03-26 17:48:08 -0700

    Removes Atlas DB

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -10,13 +10,13 @@ module.exports = {
       "user": "",
       "connector": "mongodb"
     },
-    "atlas": {
-      "host": "sdjs-speaker-shard-00-00-j2ubp.mongodb.net",
-      "port": 27017,
-      "url": process.env.MONGODB_ATLAS_URI,
-      "database": "sdjs",
-      "name": "sdjs",
-      "connector": "mongodb"
-    }
+    // "atlas": {
+    //   "host": "sdjs-speaker-shard-00-00-j2ubp.mongodb.net",
+    //   "port": 27017,
+    //   "url": process.env.MONGODB_ATLAS_URI,
+    //   "database": "sdjs",
+    //   "name": "sdjs",
+    //   "connector": "mongodb"
+    // }
   }
   
